90b1240dd07c3592cb816331704127c8fc7e45e7
Daniel Cooper <62018439+dancooper37@users.noreply.github.com>
2022-11-01 23:03:45 +0000

    added a histogram for some reason

--- main.R
+++ main.R
@@ -1,2 +1,17 @@
+library(ggplot2)
+
 wine <- read.csv("wine.csv")
+
+# Create dataframe containing frequencies of each quality rating
+quality_freq <- data.frame(seq(1, 10, 1), tabulate(wine$quality, nbins=10))
+colnames(quality_freq) <- c("Quality", "Frequency")
+
+# Doing a sneaky bar plot, but making it look like a histogram :/
+barplot(quality_freq$Frequency, 
+        main="Wine Quality Distribution",
+        xlab="Quality Score",
+        ylab="Frequency",
+        names.arg=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+        ylim=c(0, 700),
+        space = 0)
 
